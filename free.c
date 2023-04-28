66a4cebc9bcee3d5f1333b633e6203fc6b50d810
tshephang1 <tshephanghelter22@gmail.com>
2023-04-27 20:42:14 -0700

    Simple Shell Project

--- free.c
+++ free.c
@@ -1,13 +1,13 @@
 #include "andy.h"
 
 /**
- * free_all - frees all malloc'd space at end of main loop
- * @tokens: pointer to tokens array
- * @path: pointer to path variable
- * @line: pointer to user input buffer
- * @fullpath: pointer to full path
- * @flag: flag marking if full_path was malloc'd
- * Return: void
+ * free_all - frees all malloc'd space at end of main loop.
+ * @tokens: pointer to tokens array.
+ * @path: pointer to path variable.
+ * @line: pointer to user input buffer.
+ * @fullpath: pointer to full path.
+ * @flag: flag marking if full_path was malloc'd.
+ * Return: void.
  */
 void free_all(char **tokens, char *path, char *line, char *fullpath, int flag)
 {
@@ -19,10 +19,10 @@ void free_all(char **tokens, char *path, char *line, char *fullpath, int flag)
 }
 
 /**
- * free_dp - free double pointer
- * @array: double pointer to free
- * @length: length of double pointer
- * Return: void
+ * free_dp - free double pointer.
+ * @array: double pointer to free.
+ * @length: length of double pointer.
+ * Return: void.
  */
 void free_dp(char **array, unsigned int length)
 {
